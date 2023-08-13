94d2efab51db40abf6a8a8f2e9c8ffda1708dda0
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-13 19:45:42 -0300

    Arquivos da aula 11

--- javascript/aula11/ex009.js
+++ javascript/aula11/ex009.js
@@ -1,7 +1,16 @@
-var país = 'Argentina'
+function verificar(){
+    var país = document.querySelector('input#nacionalidade')
+    var res = document.querySelector('div#res')
+    var nacio = (país.value)
+    
+}
+
+/*
+var país = 'Argentina
 console.log(`Vivendo em ${país}.`)
 if (país != 'Brasil') {
     console.log('Você é estrangeiro!')
 } else {
     console.log('Você é brasileiro!')
 }
+*/
