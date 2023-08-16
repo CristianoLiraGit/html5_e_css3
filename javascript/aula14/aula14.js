623fe0cfc3309056d6f93c47744fc2544925324e
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-16 00:32:37 -0300

    update aula 14

--- javascript/aula14/aula14.js
+++ javascript/aula14/aula14.js
@@ -7,6 +7,14 @@ for(var c = 1; c <= 10; c++){
 
 */
 
+/*
+
+for(let i = 10; i >= 1; i--) { // imprimendo os números em ordem decrescente
+    console.log(i)
+}
+
+*/
+
 // Estrutura de repetição while
 /*
 let i = 0;
@@ -16,9 +24,35 @@ while(i<=10) {
 }
 */
 
-// Estrutura do while
-
+// Estrutura do while (pouco usado)
+/*
 let i = 0;
 do {
     console.log(i);
+    i++
 } while(i<10); // O teste lógico está no final
+
+// Exemplo de aplicação:
+
+let resposta ='';
+do {
+    resposta = window.prompt('Você é maior de idade?);
+} while(resposta.toLowerCase() != 'sim');
+*/
+
+// Estrutura forEach = usada em arrays
+
+// const carros = ['Fusca', 'Uno', 'Siena']
+// carros.forEach((item, i)=>{
+//     console.log(`Encontrei ${item} na posição ${i}`)
+// })
+
+/*
+const array = [1, 2, 3, 4, 5];
+
+array.forEach((element) => {
+  console.log(element);
+});
+*/
+
+console.log('Fim')
