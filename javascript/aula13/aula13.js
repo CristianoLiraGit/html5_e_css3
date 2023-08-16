ed11d3e21bb41102f6b8204ca2736c85c581a3c6
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-15 21:25:15 -0300

    aula 13
    Aula de repetição while e do while

---
+++ javascript/aula13/aula13.js
@@ -0,0 +1,18 @@
+/*
+// while
+
+var c = 1
+while(c < 5){
+    console.log(c)
+    c++
+}
+console.log('Fim do loop')
+*/
+
+// do while
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+console.log('Fim do programa')
