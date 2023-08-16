1f8913035e706c58452f76097be729c641b57adf
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-15 23:57:25 -0300

    Criei os arquivos da aula 14
    Testando outros exemplos de laços de repetição.

--- javascript/aula14/aula14.js
+++ javascript/aula14/aula14.js
@@ -1,4 +1,24 @@
 // Estrutura de repetição for
+/*
+
 for(var c = 1; c <= 10; c++){
     console.log(c)
 }
+
+*/
+
+// Estrutura de repetição while
+/*
+let i = 0;
+while(i<=10) {
+    console.log(i)
+    i++
+}
+*/
+
+// Estrutura do while
+
+let i = 0;
+do {
+    console.log(i);
+} while(i<10); // O teste lógico está no final
---
+++ javascript/aula14/node_37a0191a5d226.tmp.js
@@ -0,0 +1,15 @@
+// Estrutura de repetição for
+/*
+
+for(var c = 1; c <= 10; c++){
+    console.log(c)
+}
+
+*/
+
+// Estrutura de repetição while
+
+let i = 0;
+while(i<10) {
+    console.log(i)
+}
