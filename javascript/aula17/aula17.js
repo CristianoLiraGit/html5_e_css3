623b2c2a85f4dd9519122f090e4adad6d385607e
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-21 23:36:00 -0300

    Create aula17.js
    Aula sobre objetos no javascript.

---
+++ javascript/aula17/aula17.js
@@ -0,0 +1,11 @@
+// Objetos. 
+// Objetos são semelhantes ao arrays, porém mais avançados.
+ 
+// Para declarar objetos, usa-se {}. Ex.:
+
+let amigo = {nome: 'José', 
+sexo: 'M', 
+peso: 85.4} 
+// Os índices não serão 0, 1, 2, 3..., e sim os valores nome, sexo, peso...
+console.log(typeof amigo)
+console.log(amigo)
