0aa65ee1463e2cad8f92c5884ed4780ea430cdda
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-24 16:24:55 -0300

    ex011.js
    Fiz o exercício 011 em forma de site.

--- javascript/aula12/ex011.js
+++ javascript/aula12/ex011.js
@@ -1,8 +1,16 @@
-var idade = 15
-if (idade < 16){
-    console.log('Não vota')
-} else if (idade >= 16 && idade < 18 || idade > 65) {
-    console.log('Voto facultativo')
-} else {
-    console.log('Voto obrigatório')
+function verificar() {
+    var anos = document.getElementById('iidade')
+    var idade = anos.value
+    var res = document.querySelector('#res')
+    if (idade < 16){
+        console.log('Não vota')
+        res.innerHTML = 'Não vota'
+    } else if (idade >= 16 && idade < 18 || idade > 65) {
+        console.log('Voto facultativo')
+        res.innerHTML = 'Voto facultativo'
+    } else {
+        console.log('Voto obrigatório')
+        res.style.textAligh = 'center'
+        res.innerHTML = 'Voto obrigatório'
+    }
 }
