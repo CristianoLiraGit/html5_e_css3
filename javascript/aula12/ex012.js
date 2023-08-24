27ba2f0c67ef0d221d354382f0705a36b5aa755f
CRISTIANO BARBOSA DE LIRA <99227556+CristianoLiraGit@users.noreply.github.com>
2023-08-24 16:43:24 -0300

    Terminar 012.js
    Falta terminar

--- javascript/aula12/ex012.js
+++ javascript/aula12/ex012.js
@@ -1,15 +1,22 @@
-var hoje = new Date();
-var hora = hoje.getHours() + ':' + hoje.getMinutes() + ':' + hoje.getSeconds();
-console.log(`São exatamente ${hora}.`);
 
 // hora = 6 // Para testar
+function msg(){
+    var hoje = new Date();
+    var hora = hoje.getHours() + ':' + hoje.getMinutes() + ':' + hoje.getSeconds();
+    // console.log(`São exatamente ${hora}.`);
+    var titulo = document.getElementsByName('h1')[0]
 
-if (hora >= 0 && hora < 6){
-    console.log('Boa madrugada')
-} else if (hora >= 6 && hora < 12){
-    console.log('Bom dia')
-} else if (hora >=12 && hora < 18){
-    console.log('Boa tarde')
-} else {
-    console.log('Boa noite')
+    if (hora >= 0 && hora < 6){
+        console.log('Boa madrugada')
+        titulo.innerText = `São exatamente ${hora}.`
+    } else if (hora >= 6 && hora < 12){
+        console.log('Bom dia')
+        titulo.innerText = `São exatamente ${hora}`
+    } else if (hora >=12 && hora < 18){
+        console.log('Boa tarde')
+        titulo.innerText = `São exatamente ${hora}`
+    } else {
+        console.log('Boa noite')
+        titulo.innerText = `São exatamente ${hora}`
+    }
 }
